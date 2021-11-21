2b4c7bbc1df11a161369c29fb3787d7a9a0bdf07
kanmurimoriyasushi <kanmurimoriyasushi0@gmail.com>
2021-11-21 19:23:54 +0800

    1
    1

--- usr/kscript.js
+++ usr/kscript.js
@@ -3,57 +3,50 @@ function getET(e){return document.getElementsByTagName(e)}
 function getEI(e){return document.getElementById(e)}
 function getE(e){return document.querySelectorAll(e)}
 
-document.addEventListener('DOMContentLoaded',()=>{console.time('js');
-s=location.pathname,l=s.length,l>17?uB.textContent='…'+s.substring(l-17):uB.textContent=s;
-t=getET('title')[0];hdr.innerHTML=t.textContent+t.getAttribute('ps');
-tM.textContent=container.textContent.replace(/(\s|[\r\n])/g,'').length;
-sync.textContent=t.getAttribute('time');
-dc.textContent=getET('dfn').length+'+'+getEC('dfn').length;
-screen.width<1e3||(lBbar.style='display:none',lba.style='display:none');
-setTimeout("for(i=100;tw0.clientHeight!=tw1.clientHeight;document.body.style='zoom:'+(i--)+'%'){fSV.textContent=i}",1e3);
-screen.width<1e3&&eS.append('abbr:hover:before{top:1.5em;opacity:1;box-shadow:0 0 5px var(--sc)}');
-tN.textContent=new Date().getFullYear();
+console.time('js');
+try{localStorage.t==1&&(document.body.classList.add('night-nt'),getET('meta')[1].content='#fff')}catch{};
+document.body.style.fontFamily=localStorage.ff;
 
-try{getEC('tagMes')[0].innerHTML='<div style=padding-left:5em><div class=hrda><big style=background:var(--c1)>見符 ｢ゆっくりしていってね!｣</big></div><div style="padding:.3em 0 0 .1em;line-height:1em">①[注意]錯誤主観缺省等諸問題存在。<br>②[免責]内容造成後果不負責。<br>③[支持]排版異常縮字横屏Chrome更新下。<br>④[版権]CC<small>(此作已共享)</small>BY<small>(借物請列表)</small>NC<small>(請勿於商用)</small>。<br><em>㊟以上事(￪)＆編者(Ky)認同不能者請快関閉此頁面!</em></div></div>'}catch{};
-//try{getEC('tagLod')[0].innerHTML=`<div style=padding-left:5em><fieldset class=b-td style=border-color:#f9a825><legend class=td-b1 style=color:#f57f17>坑符「少女折寿中」</legend></fieldset>需長時間編集或一時編集終止中、或許有一天会填完。</div>`}catch{};
-//try{getEC('tagUni')[0].innerHTML=`<div style=padding-left:5em><fieldset class=b-td style=border-color:#0091ea><legend class=td-b1 style=color:#0091ea>火星「届不到的絵恋」</legend></fieldset>本頁含有專門的字符不会被日常区塊範囲的字体映照、出現的「⺚」皆為汝設備無対応絵制方法的替代絵制方法。</div>`}catch{};
+[].slice.call(getE('[getHtml]')).map(e=>{
+    fetch('../../usr/'+e.getAttribute('getHtml')).then(t=>t.text()).then(c=>
+    e.after(document.createRange().createContextualFragment(c))
+)});
 
-try{[] .slice.call(document.images).forEach(e=>{e.setAttribute('loading','lazy')})}catch{};
-//[].slice.call(document.links).forEach(e=>{e.target=e.getAttribute('href')});
-try{for(e=getET('section'),i=e.length;i>0;i--){e[i-1].id='s'+i}}catch{};
-try{for(e=getET('h1'),i=e.length;i>0;i--){e[i-1].id='h1'+i}}catch{};
-try{for(e=getEC('t2'),i=e.length;i>0;i--){e[i-1].id='h2'+i}}catch{};
 
-try{qB.innerHTML=qB.innerHTML.replace(/\n/g,'');
-for(i=qB.children.length;i>0;i--){e=document.createElement('dialog'),e.id='qD'+i,qB.appendChild(e),e.innerHTML=qB.childNodes[i-1].innerHTML}
-getE('[q]').forEach(e=>{e.onclick=()=>{f=getEI('qD'+e.getAttribute('q')),f.showModal(),f.onclick=()=>f.close()}})}catch{}
 
-/*載錯処理*///try{[].forEach.call(document.images,function(ie){ie.onerror=function(){ie.src='../usr/ie.svg';ie.classList.add('ie');iet=document.createElement('em');iet.innerText=' 〜届かないもの[LOAD ERROR]';ie.parentNode.insertBefore(iet,ie.nextSibling)}})}catch{};	
-/*創建関閉*///try{for(i=1;i<20;i++){[].forEach.call(document.querySelectorAll('#q'+i),function(qd){let qe=document.createElement('a');qe.innerText='Ｘ';qe.setAttribute('onclick','q'+i+'.close()')})}}catch{};	
-docl=setInterval(()=>{document.readyState=='complete'&&(clearInterval(docl),setTimeout("loadinger.style.opacity='0',loadinger.textContent='少女祈祷済',ldt.style.display='none'",500),setTimeout("loadinger.style.display='none'",2000))},500);
+try{[].slice.call(getEC('half')).map(e=>e.style.margin=-e.clientWidth/4+'px')}catch{}
+
+
 console.timeEnd('js');
-});
 
-onload=()=>{
-	try{iB=document.createElement('div'),
-		screen.width<1e3?(lba.appendChild(iB)):abar.appendChild(iB),
-		g=getET('section').length+1;for(i=1;i<g;i++){
-		e=document.createElement('div');e.id='l'+i;iB.appendChild(e),
-		getE('#s'+i+' h1').forEach(H1=>{e1=document.createElement('div');e1.setAttribute('i4',H1.id);e1.id='i'+H1.id;e1.textContent=H1.textContent;getEI('l'+i).appendChild(e1)});
-		getE('#s'+i+' .t2').forEach(h2=>{e2=document.createElement('div');e2.setAttribute('i4',h2.id);e2.id='i'+h2.id;e2.textContent=h2.textContent;getEI('l'+i).appendChild(e2)})}
-		iB.addEventListener('click',e=>{
-		h=getEI(e.target.getAttribute('i4')),
-		h.scrollIntoView({behavior:'smooth'}),
-		h.classList.add('target'),
-		t=setTimeout("h.classList.remove('target'),clearTimeout(t)",2e3)})
-		}catch{}
-	iO=new IntersectionObserver(f=>{f.forEach(e=>{e.isIntersecting?getEI('i'+e.target.previousSibling.id).classList.add('iOing'):getEI('i'+e.target.previousSibling.id).classList.remove('iOing')})});
-		getE('h1+p,.t2+article').forEach(h=>{iO.observe(h)});
-}
-
-function kN(){document.body.classList.contains('kN')?(document.body.classList.remove('kN'),kNi.className='ic-suns'):(document.body.classList.add('kN'),kNi.className='ic-moon')}
-function lB(){lba.hasAttribute('style')?(lba.removeAttribute('style'),lBi.className='ic-lis1'):(lba.style='display:none',lBi.className='ic-lis0')}
-function fB(){document.body.classList.contains('tf-mt')?(document.body.classList.remove('tf-mt'),fb.innerHTML='<i class=ic-font></i> MTLmr3m'):(document.body.classList.add('tf-mt'),fb.innerHTML='<i class=ic-font></i> Initial')}
+/*目录模块*/
+try{for(e=getET('section'),i=e.length-1;i>-1;i--){e[i].id='s'+i}}catch{};
+try{for(e=getET('h1'),i=e.length-1;i>-1;i--){e[i].id='H'+i}}catch{};
+try{for(e=getET('h2'),i=e.length-1;i>-1;i--){e[i].id='h'+i}}catch{};
+try{getET('main')[0].insertAdjacentHTML('beforebegin','<div id=lba><div class=lba ontouchstart=lBas() onmousedown=lBas() ontouchend=lBae() onmouseout=lBae()></div></div>');
+ d=document.createElement('div'),lba.appendChild(d);
+for(i=0,j=getET('section').length;i<j;i++){e=document.createElement('div'),e.id='l'+i,d.appendChild(e);
+ [].slice.call(getE('#s'+i+' h1')).map(m=>{n=document.createElement('div'),n.o=m.id,n.id='i'+m.id,n.textContent=m.textContent,getEI('l'+i).appendChild(n)});
+ [].slice.call(getE('#s'+i+' h2')).map(m=>{n=document.createElement('div'),n.o=m.id,n.id='i'+m.id,n.textContent=m.textContent,getEI('l'+i).appendChild(n)})}
+lba.insertAdjacentHTML('beforeend',`<hr style='margin:5px -5px 1px -5px'><div><svg onclick=history.go(-1) viewBox="0 0 24 24" width=1em><path d="M0 0h24v24H0z" fill='none'/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg> <a href=# style=all:revert;text-decoration:none;font-weight:bold;color:#000><svg viewBox="0 0 24 24" width=1em><path d="M0 0h24v24H0V0z" fill="none"/><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg></a></div>`);
+ d.addEventListener('click',m=>{n=getEI(m.target.o),n.scrollIntoView({behavior:'smooth'}),n.classList.add('target'),
+ p=setTimeout("n.classList.remove('target'),clearTimeout(p)",2e3)})
+}catch{}e=new IntersectionObserver(m=>m.map(n=>n.isIntersecting?getEI('i'+n.target.previousSibling.id).classList.add('iOing'):getEI('i'+n.target.previousSibling.id).classList.remove('iOing'))),
+ [].slice.call(getE('h1+article,h2+article')).map(m=>e.observe(m));
 function lBas(){s=new Date().getTime(),t=setTimeout("new Date().getTime()-s>100&&clearTimeout(t),lba.hasAttribute('style')?lba.removeAttribute('style'):lba.style='opacity:.8;right:calc('+(-lba.clientWidth)+'px + .45em)'",400)}
 function lBae(){clearTimeout(t),setTimeout("getEC('lba')[0].style='opacity:.2'",6e4)}
-function fS(value){document.body.style.zoom=value+'%',fSV.textContent=value}
+
+//<hr><b onclick=history.go(-1)>←</b><a href=# style=all:revert;text-decoration:none;font-weight:bold;color:#000>↑</a>
+
+//addEventListener('DOMContentLoaded',x=>{
+//    setTimeout(onLoadFun,2000);
+//   function onLoadFun(){
+
+/*
+m=setInterval(()=>title.textContent='',2e3),
+m=setInterval(()=>title.textContent='',4e3);
+*/
+
+function tT(){document.body.classList.contains('night')|document.body.classList.contains('night-nt')?
+    (localStorage.t=0,getET('meta')[1].content='#fff',document.body.classList.remove('night'),document.body.classList.remove('night-nt'),ttb.innerHTML="<path d='M3.55 18.54l1.41 1.41 1.8-1.79-1.42-1.42M11 22.45h2V19.5h-2m1-14a6 6 0 0 0-6 6 6 6 0 0 0 6 6 6 6 0 0 0 6-6c0-3.32-2.69-6-6-6m8 7h3v-2h-3m-2.76 7.66l1.8 1.79 1.41-1.41-1.79-1.8m1.79-12.28l-1.41-1.41-1.8 1.79 1.42 1.42M13 .55h-2V3.5h2m-9 7H1v2h3m2.76-7.66l-1.8-1.79-1.41 1.41 1.79 1.8 1.42-1.42z'>"):
+    (localStorage.t=1,getET('meta')[1].content='#000',document.body.classList.add('night'),ttb.innerHTML="<path d='M17.75 4.09l-2.53 1.94.91 3.06-2.63-1.81-2.63 1.81.91-3.06-2.53-1.94L12.44 4l1.06-3 1.06 3 3.19.09m3.5 6.91l-1.64 1.25.59 1.98-1.7-1.17-1.7 1.17.59-1.98L15.75 11l2.06-.05L18.5 9l.69 1.95 2.06.05m-2.28 4.95c.83-.08 1.72 1.1 1.19 1.85-.32.45-.66.87-1.08 1.27C15.17 23 8.84 23 4.94 19.07c-3.91-3.9-3.91-10.24 0-14.14.4-.4.82-.76 1.27-1.08.75-.53 1.93.36 1.85 1.19-.27 2.86.69 5.83 2.89 8.02a9.96 9.96 0 0 0 8.02 2.89m-1.64 2.02a12.08 12.08 0 0 1-7.8-3.47c-2.17-2.19-3.33-5-3.49-7.82-2.81 3.14-2.7 7.96.31 10.98 3.02 3.01 7.84 3.12 10.98.31z'>")}
